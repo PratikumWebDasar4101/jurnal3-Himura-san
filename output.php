9abf6fbd53db0df50e56af75f3a4427b40666c22
Zahid Musthofainal Akhyar <32158770+Himura-san@users.noreply.github.com>
2018-09-17 10:02:12 +0700

    Update output.php

--- output.php
+++ output.php
@@ -1,11 +1,12 @@
 <?php
-    $target_dir = "images/";
-    $target_file = $target_dir . basename($_FILES["gambar"]["name"]);
+    $gambar = $_FILES['gambar']['name'];
+    $tmp = $_FILES['gambar']['tmp_name'];
+    $dir = "images/";
+    $target_file = $dir.$gambar;
 
-    if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)) {
-        $files = basename($_FILES["gambar"]["name"]);
-        echo "<img src='images/$files' width='500px'>";
+    if (move_uploaded_file($tmp, $target_file)) {
+        echo "<img src='$target_file' width='500px'>";
     } else {
-        echo "Gagal";
+        echo "Gagal Upload..!!";
     }
 ?>
